a48430909b34ed8678cff34ae28fc70c5622e7aa
mickeyftm <84585761+mickeyftm@users.noreply.github.com>
2021-05-29 02:58:55 +0900

    Update config.ts

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -82,6 +82,21 @@ const config: MenuEntry[] = [
     icon: 'AuditIcon',
     href: 'https://solidity.finance/audits/PolyGoat/',
   },
+  export const socials = [
+    {
+      label: "Telegram",
+      icon: "TelegramIcon",
+      href: "https://t.me/berserkfinance"
+    },
+    {
+      label: "Twitter",
+      icon: "TwitterIcon",
+      href: "https://twitter.com/FinanceBerserk",
+    },
 ]
 
 export default config
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
